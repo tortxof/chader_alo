65bef9a0a39113e0a5a6805db078728a957e04bc
Daniel Jones <tortxof@gmail.com>
2015-01-01 19:03:53 -0500

    Changed to thumbnail resolution.

--- assets/js/ig-recent.js
+++ assets/js/ig-recent.js
@@ -1,7 +1,7 @@
 function igCallback (data) {
   var link = data.data[0].link;
-  var img_url = data.data[0].images.low_resolution.url;
-  var img_width = data.data[0].images.low_resolution.width;
-  var img_height = data.data[0].images.low_resolution.height;
+  var img_url = data.data[0].images.thumbnail.url;
+  var img_width = data.data[0].images.thumbnail.width;
+  var img_height = data.data[0].images.thumbnail.height;
   $('#ig-recent').find('img').attr('src', img_url);
 }
